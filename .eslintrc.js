55a0393fff209a3ab326a03f437a07d8dde85b56
joao.cardoso <jc.cardoso4@gmail.com>
2022-06-21 05:42:37 -0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+        '@vue/typescript/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'vue/html-indent': ['warn', 4],
+        'vue/html-quotes': ['warn', 'single'],
+        'vue/script-indent': ['warn', 4],
+        'vue/no-multi-spaces': 'warn',
+        'vue/html-end-tags': 'warn',
+        'vue/valid-v-slot': [0, {
+            allowModifiers: true,
+        }],
+        'class-methods-use-this': 0,
+        'lines-between-class-members': 0,
+        indent: ['warn', 4],
+        quotes: ['warn', 'single'],
+        semi: ['warn', 'always'],
+        '@typescript-eslint/no-explicit-any': [0, 'always'],
+    },
+};
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import './plugins/axios';
+import Vuetify from 'vuetify';
+import App from './App.vue';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
+import i18n from './i18n';
+
+const vuetify = new Vuetify({
+    theme: {
+        options: {
+            customProperties: true,
+        },
+        themes: {
+            light: {
+                primary: '#007BFF',
+                secondary: '#424242',
+                accent: '#82B1FF',
+                error: '#FF5252',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FFC107',
+            },
+        },
+    },
+});
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
+
+new Vue({
+    vuetify,
+    i18n,
+    render: (h) => h(App),
+}).$mount('#app');
